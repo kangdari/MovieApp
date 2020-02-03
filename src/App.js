d2d97c0cb064a422c149ac31c9408db507af5db2
kangdari_ <ksh44820@naver.com>
2020-02-03 12:18:48 +0900

    promise && axios API 호출2

--- src/App.js
+++ src/App.js
@@ -1,42 +1,52 @@
-import React, { useState, useEffect } from "react";
-import Movie from "./Components/Movie";
-import axios from "axios";
+import React, { useState, useEffect, useCallback } from 'react';
+import Movie from './Components/Movie';
+import axios from 'axios';
 
 function App() {
-  const [movies, setMovies] = useState(null);
+    const [movies, setMovies] = useState(null);
+    const url = 'https://yts.mx/api/v2/list_movies.json';
 
-  const url = "https://yts.mx/api/v2/list_movies.json";
+    // 1. axios를 이용한 API 호출
+    const callApi = async () => {
+        const response = await axios.get(url);
+        const movieSet = response.data.data.movies;
+        setMovies(movieSet);
+    };
 
-  // axios를 이용한 API 호출
-  const callApi = async () => {
-    const response = await axios.get(url);
-    const movieSet = response.data.data.movies;
-    setMovies(movieSet);
-  };
+    // 2. Promise를 이용한 API 호출
+    // useCallback으로 감싸주지 않으면 렌더링할 때마다 getMovies 함수가 생성됨.
+    // [] 렌더링될 때 한번만 실행
+    const getMovies = useCallback(async () => {
+        const movies = await APIcall();
+        setMovies(movies);
+    }, []);
 
-  useEffect(() => {
-    callApi();
-    // Promise를 이용한 API 호출
-    // fetch(url)
-    //   .then(response => response.json())
-    //   .then(json => console.log(json))
-    //   .catch(err => console.log(err))
-  }, []);
+    const APIcall = () => {
+        return fetch(url)
+            .then(response => response.json())
+            .then(response => response.data.movies)
+            .catch(err => console.log(err));
+    };
 
-  const renderMovies = movies => {
-    const moviesList = movies.map((movie, index) => {
-      return (
-        <Movie
-          key={index}
-          title={movie.title}
-          poster={movie.medium_cover_image}
-        />
-      );
-    });
-    return moviesList;
-  };
+    useEffect(() => {
+        callApi(); // 1
+        // getMovies(); // 2
+    }, []);
 
-  return <div>{!movies ? "loading" : renderMovies(movies)}</div>;
+    const renderMovies = movies => {
+        const moviesList = movies.map(movie => {
+            return (
+                <Movie
+                    key={movie.id}
+                    title={movie.title}
+                    poster={movie.medium_cover_image}
+                />
+            );
+        });
+        return moviesList;
+    };
+
+    return <div>{!movies ? 'loading' : renderMovies(movies)}</div>;
 }
 
 export default App;
